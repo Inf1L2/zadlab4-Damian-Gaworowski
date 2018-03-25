0b183cc7ea53308f82583692d2a6f7cca9e2d1ae
HP <HP@DESKTOP-LSMEN80>
2018-03-25 20:55:20 +0200

    12.03.2018

--- POLab4/src/main/java/pl/edu/ur/polab4zadanie1_2_3/Student.java
+++ POLab4/src/main/java/pl/edu/ur/polab4zadanie1_2_3/Student.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.edu.ur.polab4zadanie1_2_3;
 
 /**
@@ -56,3 +51,5 @@ public class Student {
     
     
     }
+
+  
